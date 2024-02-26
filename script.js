2b42b43839c4c229204715a66dc289cf0b9bdeca
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-26 12:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+ let bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+
+            function removeHmm(str){
+                // let newStr = str.replace(/^(The|the|a|A|An|an)\s+/g,"").trim();
+
+                let arr = ["the","The","a","A","an","An"]
+                for(let t of arr){
+                    str = str.replaceAll(t+" ","").trim()
+                }
+                return str.trim();
+            }
+
+        bands.sort((a,b)=>{
+            const nameA = removeHmm(a).toLowerCase();
+            const nameB = removeHmm(b).toLowerCase();
+            if(nameA>nameB){
+                return 1
+            }
+            else if(nameA<nameB){
+                return -1;
+            }
+            else{
+                return 0;
+            }
+
+        })
+        // console.log(bands)
+
+         let bandList = document.getElementById('band')
+    bands.forEach((value)=>{
+            let listItems = document.createElement('li')
+            listItems.innerHTML = `${value}`
+            bandList.append(listItems)
+        })
